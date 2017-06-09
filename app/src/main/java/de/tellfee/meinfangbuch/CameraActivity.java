821b8ba695d7fa132cbcbd310bca9319f85b8761
Lukas <lukas160394@web.de>
2017-06-09 16:49:33 +0200

    Camera Preview erweitert

--- app/src/main/java/de/tellfee/meinfangbuch/CameraActivity.java
+++ app/src/main/java/de/tellfee/meinfangbuch/CameraActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Camera;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -12,6 +14,7 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.IntDef;
@@ -32,8 +35,12 @@ public class CameraActivity extends AppCompatActivity {
     private TextureView tv_preview;
 
     private Size previewSize;
+    private Size previewSizeScaled;
     private double previewRatio;
+    private Size streamSize;
+    private double streamRatio;
 
+    private SurfaceTexture surfaceTexture;
     private CameraDevice cameraDevice;
     private CameraManager manager;
     private String camID;
@@ -50,6 +57,8 @@ public class CameraActivity extends AppCompatActivity {
     private TextureView.SurfaceTextureListener surfaceTextureListener   = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            previewSize     = new Size(width, height);
+            previewRatio    = (double)width / (double)height;
             openCamera();
         }
 
@@ -68,7 +77,38 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeUIImmersive();
+
+        setContentView(R.layout.activity_camera);
+
+        tv_preview = (TextureView) findViewById(R.id.tv_preview);
+        tv_preview.setSurfaceTextureListener(surfaceTextureListener);
+
+        Log.e("CAMERA_ACTIVITY", "onResume");
+        startBackgroundThread();
+        if (tv_preview.isAvailable()) {
+            openCamera();
+        } else {
+            tv_preview.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        Log.e("CAMERA_ACTIVITY", "onPause");
+        //closeCamera();
+        stopBackgroundThread();
+        super.onPause();
+    }
+
+
+    private void makeUIImmersive(){
         getSupportActionBar().hide();
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -76,14 +116,8 @@ public class CameraActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-
-        setContentView(R.layout.activity_camera);
-
-        tv_preview = (TextureView) findViewById(R.id.tv_preview);
-        tv_preview.setSurfaceTextureListener(surfaceTextureListener);
-
-
     }
+
 
     private void openCamera() {
         manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
@@ -92,7 +126,10 @@ public class CameraActivity extends AppCompatActivity {
             for (String cam : cams) {
                 CameraCharacteristics c = manager.getCameraCharacteristics(cam);
                 if (c.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
-                    camID = cam;
+                    camID                       = cam;
+                    StreamConfigurationMap map  = c.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    streamSize                  = map.getOutputSizes(ImageFormat.JPEG)[0];
+                    streamRatio                 = streamSize.getWidth() / streamSize.getHeight();
                 }
             }
 
@@ -114,7 +151,7 @@ public class CameraActivity extends AppCompatActivity {
                     public void onError(@NonNull CameraDevice camera, int error) {
 
                     }
-                }, backgroundHandler);
+                }, null);
             }
         } catch (CameraAccessException e) {
             e.printStackTrace();
@@ -122,8 +159,10 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void createCameraPreview() {
-        SurfaceTexture texture  = tv_preview.getSurfaceTexture();
-        Surface surface         = new Surface(texture);
+        surfaceTexture          = tv_preview.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(streamSize.getWidth(), streamSize.getHeight());
+//        surfaceTexture.setDefaultBufferSize(1080, 1080);
+        Surface surface         = new Surface(surfaceTexture);
 
         try {
             streamCaptureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
@@ -144,19 +183,61 @@ public class CameraActivity extends AppCompatActivity {
                 public void onConfigureFailed(@NonNull CameraCaptureSession session) {
                     Toast.makeText(CameraActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
                 }
-            }, backgroundHandler);
+            }, null);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
     private void updatePreview() {
+        previewSizeScaled   = getPreviewSizeScaled();
+//        Rect rect           = getPreviewRect();
+//        surfaceTexture.setDefaultBufferSize(previewSizeScaled.getWidth(), previewSizeScaled.getHeight());
+
         streamCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+//        streamCaptureRequestBuilder.set(CaptureRequest.SCALER_CROP_REGION, rect);
         try {
             streamCaptureSession.setRepeatingRequest(streamCaptureRequestBuilder.build(), null, backgroundHandler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private Rect getPreviewRect(){
+        Rect r = new Rect();
+//        Size size       = previewSizeScaled;
+        Size size       = new Size(1080, 1080);
+        Log.e("PREVIEW-SIZE", ""+previewSize);
+        Log.e("SIZE", ""+previewSizeScaled);
+
+        Log.e("SCALED-RATIO", ""+(double)previewSizeScaled.getHeight() / (double)previewSizeScaled.getWidth());
+        Log.e("RATIO", ""+(double)previewSize.getHeight() / (double)previewSize.getWidth());
+
+        r.set(
+                (streamSize.getWidth() - size.getHeight()) / 2,
+                (streamSize.getHeight() - size.getWidth()) / 2,
+                ((streamSize.getWidth() - size.getHeight()) / 2) + size.getHeight() - 1,
+                ((streamSize.getHeight() - size.getWidth()) / 2) + size.getWidth() - 1
+        );
+        Log.e("RECT", ""+r);
+        return r;
+    }
+
+
+    private Size getPreviewSizeScaled() {
+        int width;
+        double tmpRatio = (double)previewSize.getHeight() / (double)previewSize.getWidth();
+        if(tmpRatio > streamRatio){
+            width   = streamSize.getHeight();
+        }else{
+            width   = streamSize.getWidth();
+        }
+        int height      = (int)(width * (1 / previewRatio));
+        Size newSize    = new Size(width, height);
+
+        return newSize;
+
+
     }
 
 
@@ -184,6 +265,7 @@ public class CameraActivity extends AppCompatActivity {
         try {
             backgroundThread.join();
             backgroundThread = null;
+            backgroundHandler.getLooper().quit();
             backgroundHandler = null;
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -202,25 +284,6 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e("CAMERA_ACTIVITY", "onResume");
-        startBackgroundThread();
-        if (tv_preview.isAvailable()) {
-            openCamera();
-        } else {
-            tv_preview.setSurfaceTextureListener(surfaceTextureListener);
-        }
-    }
 
-
-    @Override
-    protected void onPause() {
-        Log.e("CAMERA_ACTIVITY", "onPause");
-        //closeCamera();
-        stopBackgroundThread();
-        super.onPause();
-    }
 
 }
